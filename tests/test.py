d41b619385da5ab54818d33b8df6621f3f20f540
Axelfoley <axel.roemer@posteo.de>
2021-03-21 23:34:06 +0100

    cleanup; use parametrized fixtures in pytest

--- tests/test.py
+++ tests/test.py
@@ -1,14 +1,10 @@
 #!/usr/bin/env python3
 
 from backend import bowling_module
-from logging import info, error, getLogger, debug
-import logging
-logging.basicConfig(level = logging.INFO)
 import pytest
 
-logger = getLogger(__name__)
-bs_simple = bowling_module.BowlingScore()
-bs_strike = bowling_module.BowlingScore()
+bs_simple_example = bowling_module.BowlingScore()
+bs_specific_example = bowling_module.BowlingScore()
 
 class Test_single_scores:
     """ test output values depending on input
@@ -35,7 +31,6 @@ class Test_single_scores:
         assert score == 10
         assert frameCount == 2
         assert textOutput == 'Strike!'
-
 
 class Test_spare:
     """ test spare event
@@ -94,116 +89,49 @@ class Test_error:
         assert score == 3
         assert frameCount == 1
 
-class Test_simple_full_example:
+class Test_simple_example:
     """ run through a full game with no spares and no strikes
     """
 
-    def test(self, *args):
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 1
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 2
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 3
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 4
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 5
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 6
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 7
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 8
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 9
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 10
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 11
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 12
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 13
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 14
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 15
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 16
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 17
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 18
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 19
-        score, frameCount, textOutput = bs_simple.calculateScore(1)
-        assert score == 20
+    @pytest.mark.parametrize("expectedScore", range(1, 21))
+    def test_simple_example(self, expectedScore):
+        score, frameCount, textOutput = bs_simple_example.calculateScore(1)
+        assert score == expectedScore
 
-class Test_example:
+class Test_specific_example:
     """ test full example
     """
 
-    def test(self, *args):
-        score, frameCount, textOutput = bs_strike.calculateScore(1)
-        assert score == 1
-        score, frameCount, textOutput = bs_strike.calculateScore(4)
-        assert score == 5
+    @pytest.mark.parametrize(
+        "input, expectedScore",
+        [
+            (1, 1),
+            (4, 5),
+            (4, 9),
+            (5, 14),
+            (6, 20),
+            (4, 24),
+            (5, 34),
+            (5, 39),
+            (10, 59),
+            (0, 59),
+            (1, 61),
+            (7, 68),
+            (3, 71),
+            (6, 83),
+            (4, 87),
+            (10, 107),
+            (2, 111),
+            (8, 127),
+            (6, 133),
+        ]
+    )
+    def test_specific_example(self, input, expectedScore):
+        score, frameCount, textOutput = bs_specific_example.calculateScore(input)
+        assert score == expectedScore
 
-        score, frameCount, textOutput = bs_strike.calculateScore(4)
-        assert score == 9
-        score, frameCount, textOutput = bs_strike.calculateScore(5)
-        assert score == 14
-
-        # spare
-        score, frameCount, textOutput = bs_strike.calculateScore(6)
-        assert score == 20
-        score, frameCount, textOutput = bs_strike.calculateScore(4)
-        assert score == 24
-
-        # spare
-        score, frameCount, textOutput = bs_strike.calculateScore(5)
-        assert score == 34
-        score, frameCount, textOutput = bs_strike.calculateScore(5)
-        assert score == 39
-
-        # strike
-        score, frameCount, textOutput = bs_strike.calculateScore(10)
-        assert score == 59
-
-        score, frameCount, textOutput = bs_strike.calculateScore(0)
-        assert score == 59
-        score, frameCount, textOutput = bs_strike.calculateScore(1)
-        assert score == 61
-
-        # spare
-        score, frameCount, textOutput = bs_strike.calculateScore(7)
-        assert score == 68
-        score, frameCount, textOutput = bs_strike.calculateScore(3)
-        assert score == 71
-
-        # spare
-        score, frameCount, textOutput = bs_strike.calculateScore(6)
-        assert score == 83
-        score, frameCount, textOutput = bs_strike.calculateScore(4)
-        assert score == 87
-
-        # strike
-        score, frameCount, textOutput = bs_strike.calculateScore(10)
-        assert score == 107
-
-        # spare
-        score, frameCount, textOutput = bs_strike.calculateScore(2)
-        assert score == 111
-        score, frameCount, textOutput = bs_strike.calculateScore(8)
-        assert score == 127
-        score, frameCount, textOutput = bs_strike.calculateScore(6)
-        assert score == 133
-
-        # finally make sure game ends
-        assert textOutput == "Game over!"
-
+    def test_makeSureGameEnds(self, *args):
         # make sure user cant proceed with further input
-        score, frameCount, textOutput = bs_strike.calculateScore(6)
+        score, frameCount, textOutput = bs_specific_example.calculateScore(6)
         assert score == 133
         assert textOutput == "Game over!"
